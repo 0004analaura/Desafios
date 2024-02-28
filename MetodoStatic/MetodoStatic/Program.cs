1c460786ff951724f37aab5aa71de4ed3e42f728
Ana Laura Oliva <aolivac4@miumg.edu.gt>
2024-02-27 23:05:34 -0600

    mas desafios

---
+++ MetodoStatic/MetodoStatic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class HelloWord
+{
+    static void Main()
+    {
+        CalculoDeMeses(5000, 2500, 3000);
+        Console.ReadKey(true);
+    }
+
+    public static void CalculoDeMeses(int mesUno, int mesDos, int mesTres)
+
+    {
+        string nombre;
+
+        int suma = mesUno + mesDos + mesTres;
+        int promedio = suma / 3;
+        Console.WriteLine("Ingrese su nombre");
+        nombre = Console.ReadLine();
+        Console.WriteLine("Hola {0} en total ganaste {1} y promediaste {2}", nombre, suma, promedio);
+
+    }
+}
+
+
+
---
+++ operadores/operadores/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Calculadora operaciones = new Calculadora();
+
+            Console.WriteLine("suma:" + operaciones.suma(1, 2));
+            Console.WriteLine("resta:" + operaciones.resta(1, 2));
+            Console.WriteLine("multiplicacion" + operaciones.multiplicacion(1, 2));
+            Console.WriteLine("division" + operaciones.division(1, 2));
+
+            Console.ReadKey(true);
+        }
+
+
+        class Calculadora
+        {
+            public int suma(int num1, int num2) {
+                return num1 + num2;
+            }
+
+            
+            public int resta(int num1, int num2)
+            {
+                return num1 - num2;
+            }
+
+            public int multiplicacion(int num1, int num2)
+            {
+                return num1 * num2;
+            }
+
+            public int division(int num1, int num2)
+            {
+                return num1 / num2;
+            }
+
+        }
+    }
+}
